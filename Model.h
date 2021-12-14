6b18ee86d5fe896434cdc15db0bbf05b9f3488c4
Jex97 <746806139@qq.com>
2021-12-14 16:32:37 +0800

    Update Model.h

--- Model.h
+++ Model.h
@@ -8,7 +8,7 @@ private:
 	std::vector<Vec3f> vertices_;      // 顶点;
 	std::vector<std::verctor<int>> faces_;  //面片
 public:
-	Model(const string& filename);
+	Model(const std::string& filename);
 	~Model();
 	int nvertices;           // 顶点的数量
 	int nfaces;				 // 面片的数量
